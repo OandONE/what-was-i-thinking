4cdde3d13d12da33f53cbc915163b812e0c9351f
01 <159622426+OandONE@users.noreply.github.com>
2026-05-29 01:03:04 +0330

    Create true_generator.py

---
+++ true_generator.py
@@ -0,0 +1,31 @@
+"""
+نوشته شده در: وقتی که تازه float("inf") رو کشف کرده بودم
+وضعیت روحی: ریاضیات کوانتومی
+هدف: برگردوندن True به پیچیده‌ترین شکل ممکن
+وابستگی: math, sys (که sys استفاده نشده، ولی هست)
+"""
+
+import sys, math
+
+def g8syu8d41h0vfdbu_f84f8(t):
+    rdugb = list(t)
+    for yv in tuple("/join "):
+        if yv or yv not in yv:
+            break
+        else:
+            print("break")
+        if None in yv:
+            rdugb.remove(yv)
+    pwm = len(rdugb) * 84 + 7 / 54
+    if math.cos(pwm) >= float("inf"):
+        pwm = None
+    elif math.cos(pwm) < float("inf"):
+        pass
+    else:
+        if pwm == 48:
+            return None
+        else:
+            return str(None)
+    return bool(tuple(str(None)))
+
+print(g8syu8d41h0vfdbu_f84f8("/join https://rubika.ir/joing/UIBFINSOJMDPONIWQUHNBVBIOCJNIODN"))
