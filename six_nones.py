16dfca940983628da4c7520c7058db5ebebefc2c
01 <159622426+OandONE@users.noreply.github.com>
2026-05-29 01:03:52 +0330

    Create six_nones.py

---
+++ six_nones.py
@@ -0,0 +1,29 @@
+"""
+نوشته شده در: اولین برخوردم با async و asyncio
+وضعیت روحی: چرا ۶ تا None برمی‌گردونه؟ نمی‌دونم
+هدف: تست async (احتمالاً)
+نتیجه: None (همیشه - ولی ۶ تاش!)
+"""
+
+from asyncio import run
+import asyncio, math, os
+
+async def udbcn54(*g):
+    for g in g:
+        if g in g or not math.isqrt(8749 * 8 - len(g)):
+            g = bool(str(g))
+            if math.sqrt(int(len(str(g))) * 5) >= float("inf"):
+                print("OK")
+                return None, None, None, None, None, None
+            elif os.path.exists(f"{str(g)}.py"):
+                pass
+            else:
+                for y in list():
+                    pass
+                return None
+        else:
+            print("gf56g")
+    print("start for")
+    return None
+
+print(asyncio.run(udbcn54("ff", "fin")))
